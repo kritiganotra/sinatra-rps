bb57a17e063ef61fdece59fbaf71c066222d2675
kritiganotra <kritiganotra@gmail.com>
2025-01-29 05:16:45 +0000

    Finished the RPS challenge

--- app.rb
+++ app.rb
@@ -2,8 +2,50 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
 end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+  if @comp_move == "paper"
+    @result = "We lost!"
+  elsif @comp_move == "scissors"
+    @result = "You won!"
+  else 
+    @result = "We tied!"
+  end
+
+  erb(:rock)
+end
+
+get ("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @result = "We lost!"
+  elsif @comp_move == "paper"
+    @result = "You won!"
+  else 
+    @result = "We tied!"
+  end
+
+  erb(:scissors)
+end 
+
+get ("/paper") do
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+
+  if @comp_move == "scissors"
+    @result = "We lost!"
+  elsif @comp_move == "rock"
+    @result = "You won!"
+  else 
+    @result = "We tied!"
+  end
+
+  erb(:paper)
+end  
